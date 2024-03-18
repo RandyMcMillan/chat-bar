72be844366e6cc5e4dd8f27ccce9e10636e9e5c9
Crescent617 <lihuaru617@outlook.com>
2024-03-18 17:53:03 +0800

    refine: msg

--- chat-bar/src/msg.rs
+++ chat-bar/src/msg.rs
@@ -3,8 +3,13 @@ use std::fmt::Display;
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
-pub(crate) static HOSTNAME: Lazy<String> =
-    Lazy::new(|| hostname::get().unwrap().to_string_lossy().to_string());
+pub(crate) static USER_NAME: Lazy<String> = Lazy::new(|| {
+    format!(
+        "{}",
+        std::env::var("USER")
+            .unwrap_or_else(|_| hostname::get().unwrap().to_string_lossy().to_string()),
+    )
+});
 
 #[derive(Clone, Copy, Debug, Serialize, Deserialize, Default)]
 pub enum MsgKind {
@@ -26,7 +31,7 @@ pub struct Msg {
 impl Default for Msg {
     fn default() -> Self {
         Self {
-            from: HOSTNAME.clone(),
+            from: USER_NAME.clone(),
             content: "".to_string(),
             kind: MsgKind::Chat,
         }
@@ -60,11 +65,7 @@ impl<'a> From<&'a Msg> for ratatui::text::Line<'a> {
                 Color::LightCyan,
                 // Color::White,
             ];
-            use std::collections::hash_map::DefaultHasher;
-            use std::hash::{Hash, Hasher};
-            let mut hasher = DefaultHasher::new();
-            s.hash(&mut hasher);
-            let h = hasher.finish() as usize;
+            let h = s.bytes().fold(0, |acc, b| acc ^ b as usize);
             return LIGHT_COLORS[h % LIGHT_COLORS.len()];
         }
 
@@ -77,7 +78,11 @@ impl<'a> From<&'a Msg> for ratatui::text::Line<'a> {
             )),
             Chat => Line::default().spans(vec![
                 Span::styled(
-                    format!("{}: ", m.from),
+                    format!(
+                        "{}{}: ",
+                        m.from,
+                        if m.from == *USER_NAME { " (You)" } else { "" }
+                    ),
                     Style::default().fg(gen_color_by_hash(&m.from)),
                 ),
                 m.content.clone().into(),
@@ -92,17 +97,7 @@ impl Display for Msg {
         match self.kind {
             MsgKind::Join => write!(f, "{} join", self.from),
             MsgKind::Leave => write!(f, "{} left", self.from),
-            MsgKind::Chat => write!(
-                f,
-                "{}{}: {}",
-                self.from,
-                if self.from.eq(&*HOSTNAME) {
-                    " (You)"
-                } else {
-                    ""
-                },
-                self.content
-            ),
+            MsgKind::Chat => write!(f, "{}: {}", self.from, self.content),
             MsgKind::System => write!(f, "[System] {}", self.content),
             MsgKind::Raw => write!(f, "{}", self.content),
         }
